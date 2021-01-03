732cc87b0382570ca10de32061d025a98e6a3b6b
luis muñoz <munozrodas.luisalfredo@gmail.com>
2021-01-02 23:37:30 -0500

    estilizando el margen del pdf y las imagenes

--- src/app/components/generar-pdf/generar-pdf.component.ts
+++ src/app/components/generar-pdf/generar-pdf.component.ts
@@ -30,7 +30,7 @@ export class GenerarPdfComponent implements OnInit {
     result => this.logoDataUrl = result
     )
 
-    Utils.getImageDataUrlFromLocalPath1('assets/firma.png').then(
+    Utils.getImageDataUrlFromLocalPath1('assets/firma2.png').then(
       result => this.firmaUrl = result
       )
 
@@ -145,6 +145,86 @@ export class GenerarPdfComponent implements OnInit {
   }
 
   
+
+
+  probandoPdf(){
+
+    const title = [ 
+      {text: 'N°', alignment: 'center', fontSize: 10}, 
+      {text: 'APELLIDOS Y NOMBRES',alignment: 'center', fontSize: 10},
+      {text: 'C.E/DNI/PAS/RUC', colSpan: 2, alignment: 'center', fontSize: 10}, 
+      {}
+    ]
+    const subhead= [{text: 'SEDE : CONSTRUCCIÓN', colSpan: 4, alignment: 'left', fontSize: 12}, {}, {},{}]
+
+    this.body.push(title, subhead);
+    let val = 0;
+    for (let i=val; i<this.work.length; i++){
+    
+        this.body.push([i+1, this.work[i][0],'DNI',this.work[i][1]]);       
+        
+    }
+
+
+    const documentDefinition = {
+
+  content: [
+	
+    
+		{
+			stack: [
+          {
+            
+              image: this.logoDataUrl, width: 200, height: 35, margin: [ 10 , 2 , 0 , 0 ] ,
+        
+              
+            
+          },
+          { text: 'SCTR5361933-S0237129-SALUD', fontSize: 9, alignment: 'left', margin: [ 0 , 15 , 10 , 10 ]},
+          { text: 'Miraflores, 18 de Diciembre del 2020', fontSize: 9,alignment: 'right', margin: [ 0 , 2 , 0 , 10 ]},
+          { text: '07:43 AM', fontSize: 9, decoration:'underline', alignment: 'right', margin: [ 0 , 2 , 0 , 10 ]},
+
+          { text: 'CONSTANCIA', fontSize: 11, decoration:'underline', alignment: 'center', margin: [ 5 , 2 , 10 , 20 ]},
+          { text:'Por medio de la presente, dejamos constancia que los Señores:', fontSize:9, alignment: 'left', margin: [ 0 , 4 , 0 , 5 ]},
+          { text:'C & R CLERQUE CONSTRUCTORA PERU SOCIEDAD ANONIMA CERRADA', fontSize:12, alignment: 'center', margin: [ 0 , 5 , 0 , 5 ]},
+          { text:'De acuerdo a lo establecido en el Decreto Supremo 003-98-SA – Normas Técnicas del Seguro Complementario de Trabajo de Riesgo, a la fecha han contratado con Rimac S.A. Entidad Prestadora de Salud la(s) póliza(s) de Seguro Complementario de Trabajo de Riesgo siguiente(s):', fontSize:9, alignment: 'justify'},
+          { text: 'SCTR SALUD  N° S0237129', fontSize: 11, alignment: 'center', margin: [ 0 , 5 , 0 , 3 ]},
+          { text: 'La constancia es de vigencia mensual y es renovable', fontSize: 9,alignment: 'center', margin: [ 0 , 1 , 0 , 6 ]},
+          { text: 'La presente constancia tiene vigencia hasta el 31.12.2020. A solicitud de la empresa contratante se emite la presente Constancia detallando a continuación el personal que se encuentra afiliado a la(s) póliza(s) antes mencionada(s).', fontSize: 9, alignment: 'justify'},
+
+          { text: 'RELACION DE PERSONAL:', fontSize: 11, alignment: 'left', margin: [ 0 , 20 , 0 , 0 ]},
+
+          {
+            
+            table: {
+              widths: [30, 270, 70 ,70],            
+              body: this.body           
+              
+            }
+          },
+          { text: 'Se expide la presente a solicitud del Asegurado/Contratante para los fines que estime convenientes.', fontSize: 9,alignment: 'left', margin: [ 2 , 10 , 0 , 6 ]},
+          
+          {image: this.firmaUrl, margin: [ 10 , 7 , -10 , 0 ], alignment: 'right', width: 120, height: 60},
+          { text: 'Mark Andrés Reyes Ploog', fontSize: 10, alignment: 'right', margin: [ 0 , 0 , -4 , 0]},
+          { text: 'Rimac EPS S.A. Entidad Prestadora ', fontSize: 10, alignment: 'right', margin: [ 0 , 0 , -24 , 0]},
+          { text: 'Usuario :  CR1RZAMUDM', fontSize: 8, alignment: 'left', margin: [ 0 , 0 , 0 , 0]},
+          { text: 'de Salud', fontSize: 10, alignment: 'right', margin: [ 0 , -10 , 35 , 0]},
+          
+          
+      ],
+      margin: [33, 0, 33, 0],
+			
+    },
+
+	],
+	
+
+
+
   
 
+    };
+    pdfMake.createPdf(documentDefinition).open();
+
+  }
 }
